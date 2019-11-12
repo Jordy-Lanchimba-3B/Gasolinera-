80cea0ad874c75e6be03643dc3508547d087748f
Jordy-Lanchimba-3B <56418626+Jordy-Lanchimba-3B@users.noreply.github.com>
2019-11-12 16:52:03 -0500

    Add files via upload

---
+++ Gasolinera/Gasolinera/Clases/Cliente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gasolinera.Clases
+{
+    public class Cliente
+    {
+        public Cliente(string nombre, string cedula, string direccion)
+        {
+            Nombre = nombre;
+            Cedula = cedula;
+            Direccion = direccion;
+        }
+        public Cliente()
+        {
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string cedula;
+
+        public string Cedula
+        {
+            get { return cedula; }
+            set { cedula = value; }
+        }
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private Factura factura;
+
+        public Factura Factura
+        {
+            get { return factura; }
+            set { factura = value; }
+        }
+    }
+}
---
+++ Gasolinera/Gasolinera/Clases/Gasolina.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gasolinera.Clases
+{
+    public class Gasolina
+    {
+        public Gasolina(string tipo, int cantidadGalones, double precio)
+        {
+            Tipo = tipo;
+            CantidadGalones = cantidadGalones;
+            Precio = precio;
+        }
+
+        public Gasolina()
+        {
+        }
+
+        private string tipo;
+
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private double cantidadGalones;
+
+        public double CantidadGalones
+        {
+            get { return cantidadGalones; }
+            set { cantidadGalones = value; }
+        }
+
+        private double precio;
+
+        public double Precio
+        {
+            get
+            {
+                return precio;
+            }
+            set { precio = value; }
+        }
+
+        private Factura factura;
+
+        public Factura Factura
+        {
+            get { return factura; }
+            set { factura = value; }
+        }
+    }
+}
